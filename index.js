c0618c9404bfd16d61ff0dcc7d7f0faeb1bf6bcd
josephmotha1998 <josephmotha@outlook.com>
2020-10-30 14:27:15 -0400

    added time table entry function
    student can now search for time table entry with or without course compoenent

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const port = 3000
 app.use('/',express.static('static'));
 
 // get list of classes
-app.get('/api/courses', (req, res) => {
-    console.log(`GET request from ${req.url}`);
-    res.send(courses);
-});
-
 app.get('/api/courses/all', (req, res) => {
     console.log(`GET request from ${req.url}`);
     const allCourses = []
@@ -24,19 +19,49 @@ app.get('/api/courses/all', (req, res) => {
     res.send(allCourses);
 });
 
+// get courses codes by subject
 app.get('/api/courses/:subject', (req, res) => {
     console.log(`GET request from ${req.url}`);
-    // const course_codes = courses.find(c => c.subject === req.params.subject);
+    // filter course codes
     course_codes = []
     for(course of courses){
         if(req.params.subject === course["subject"]){
             course_codes.push(course["catalog_nbr"].toString())
         }
     }
-    if (!course_codes){
+    if (course_codes.length === 0){
         res.status(404).send('subject was not found')
     }
     res.send(course_codes)
+});
+
+// get time table entry by subject, course code and component
+app.get('/api/courses/:subject/:course_code/:course_component?', (req, res) => {
+    console.log(`GET request from ${req.url}`);
+    // filter course codes
+    tableEntry = []
+    if(!req.params.course_component){
+
+        for(course of courses){
+            if(req.params.subject === course["subject"] && req.params.course_code === course["catalog_nbr"].toString() ){
+                tableEntry.push(course["course_info"])
+            }
+        }
+    }
+    else{
+        for(course of courses){
+            if(req.params.subject === course["subject"] && req.params.course_code === course["catalog_nbr"].toString()){
+                for(component of course["course_info"]){
+                    if (req.params.course_component === component["ssr_component"])
+                        tableEntry.push(component)
+                }
+            }
+        }
+    }
+    if (tableEntry.length === 0){
+        return res.status(404).send('the course code or subject does not exist')
+    }
+    res.send(tableEntry)
 });
 
 app.listen(port, () => {
