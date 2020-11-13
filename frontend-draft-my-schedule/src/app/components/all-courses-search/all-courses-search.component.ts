c57614d46985fda13a1ea22fa8a6eecbe8c32690
josephmotha1998 <josephmotha@outlook.com>
2020-11-13 10:52:21 -0500

    Displays all courses
    Now displays all courses

--- frontend-draft-my-schedule/src/app/components/all-courses-search/all-courses-search.component.ts
+++ frontend-draft-my-schedule/src/app/components/all-courses-search/all-courses-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { allCourse } from 'src/app/models/allCourse';
 import { AllCoursesService } from 'src/app/services/all-courses.service';
 
@@ -8,12 +8,14 @@ import { AllCoursesService } from 'src/app/services/all-courses.service';
   styleUrls: ['./all-courses-search.component.css']
 })
 export class AllCoursesSearchComponent implements OnInit {
+  @Input() course:allCourse;
   allCourses: allCourse[];
   constructor(private AllCoursesService:AllCoursesService) { }
 
   ngOnInit(): void {
     this.AllCoursesService.getCourses().subscribe(allCourses => {
       this.allCourses = allCourses
+      console.log(allCourses[0])
     }
 
     );
